d0645771486bc0293db92847fca13562d4e8e2a7
Alciver08 <alciver08@gmail.com>
2024-08-22 16:19:33 -0500

    variables

--- src/enviroments/environments.ts
+++ src/enviroments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrlCrear: 'http://backend-node:3000/registros',  // Cambiar el host a backend-node
+  apiUrlCrear: 'http://localhost:3000/api/backend-nodejs/personas',  // Cambiar el host a backend-node
   apiUrlEliminar: 'http://backend-node:3000/registros/{id}' // Cambiar el host a backend-node
 };
